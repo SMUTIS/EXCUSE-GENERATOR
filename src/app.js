319d8d717c48e27bfa1eb2266804efd8cadc6ca6
SMUTIS <sandramutissanabria@gmail.com>
2021-08-10 20:00:06 +0000

    inicial

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function random() {
+  var quienes = [
+    "Ramona",
+    "Batman",
+    "Chiquito",
+    "Gollum",
+    "Trump",
+    "Spiderman",
+    "mi gato"
+  ];
+  var accion = [
+    "me rompió la aspiradora",
+    "se comio mi practica de JS",
+    "tiro el ordenador por la ventana",
+    "declaro la 3era guerra mundial",
+    "secuestro a mi gato",
+    "me robo la cartera",
+    "me rasguño la cara"
+  ];
+  var cuando = [
+    "antes de clase",
+    "mientras dormia",
+    "antes de comer",
+    "después de trabajar",
+    "en mis sueños",
+    "mientras compraba",
+    "cuando le daba de comer"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var quienesalea = quienes[Math.floor(Math.random() * (7 - 0)) + 0];
+  var accionalea = accion[Math.floor(Math.random() * (7 - 0)) + 0];
+  var cuandoalea = cuando[Math.floor(Math.random() * (7 - 0)) + 0];
+
+  var concatenar = quienesalea + " " + accionalea + " " + cuandoalea;
+  return concatenar;
+}
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = random();
+  });
 };
